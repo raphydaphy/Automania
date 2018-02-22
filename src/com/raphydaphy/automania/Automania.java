8cfcff751a95cd76ad521626d685417fbc1ccb7a
RAPH HENNESSY <raph.hennessy@gmail.com>
2018-02-22 15:09:29 +1100

    Re-do it all

--- src/com/raphydaphy/automania/Automania.java
+++ src/com/raphydaphy/automania/Automania.java
@@ -1,20 +1,14 @@
 package com.raphydaphy.automania;
 
-import com.raphydaphy.automania.math.Vector2f;
-import com.raphydaphy.automania.render.Renderer;
-import com.raphydaphy.automania.render.Texture;
+import com.raphydaphy.automania.math.Matrix4f;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 
-import java.awt.*;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.logging.Level;
@@ -38,12 +32,14 @@ public class Automania
 	};
 	private long window;
 	private GameTimer timer;
-	private Renderer renderer;
 
-	private int width;
-	private int height;
+	private IntBuffer width;
+	private IntBuffer height;
 
-	private Texture example;
+	private int vao;
+	private int vbo;
+
+	private int program;
 
 	public static boolean isGL32()
 	{
@@ -55,7 +51,6 @@ public class Automania
 		System.out.println("Ver: " + Version.getVersion());
 
 		timer = new GameTimer();
-		renderer = new Renderer();
 
 		GLFW.glfwSetErrorCallback(errorCallback);
 
@@ -73,10 +68,13 @@ public class Automania
 
 		window = GLFW.glfwCreateWindow(640, 480, "Automania", MemoryUtil.NULL, MemoryUtil.NULL);
 
+		// OpenGL 3.2 not supported
 		if (window == MemoryUtil.NULL)
 		{
-			GLFW.glfwTerminate();
-			throw new RuntimeException("Could not create the window, sorry :(");
+			GLFW.glfwDefaultWindowHints();
+			GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 2);
+			GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 1);
+			window = GLFW.glfwCreateWindow(640, 480, "Autonamia", MemoryUtil.NULL, MemoryUtil.NULL);
 		}
 
 		GLFW.glfwSetKeyCallback(window, keyCallback);
@@ -85,33 +83,85 @@ public class Automania
 		GL.createCapabilities();
 
 		timer.init();
-		renderer.init();
 
-		int width, height;
-		try (MemoryStack stack = MemoryStack.stackPush()) {
-			long window = GLFW.glfwGetCurrentContext();
-			IntBuffer widthBuffer = stack.mallocInt(1);
-			IntBuffer heightBuffer = stack.mallocInt(1);
-			GLFW.glfwGetFramebufferSize(window, widthBuffer, heightBuffer);
-			width = widthBuffer.get();
-			height = heightBuffer.get();
+		width = MemoryUtil.memAllocInt(1);
+		height = MemoryUtil.memAllocInt(1);
+
+		vao = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vao);
+
+		try (MemoryStack stack = MemoryStack.stackPush())
+		{
+			FloatBuffer vertices = stack.mallocFloat(3 * 6);
+			vertices.put(-0.6f).put(-0.4f).put(0f).put(1f).put(0f).put(0f);
+			vertices.put(0.6f).put(-0.4f).put(0f).put(0f).put(1f).put(0f);
+			vertices.put(0f).put(0.6f).put(0f).put(0f).put(0f).put(1f);
+			vertices.flip();
+
+			vbo = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STATIC_DRAW);
 		}
 
-		example = Texture.loadTexture("resources/example.png");
+		int vertexShader = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+		GL20.glShaderSource(vertexShader, "#version 150 core\n" + "\n" + "in vec3 position;\n" + "in vec3 color;\n" + "\n" + "out vec3 vertexColor;\n" + "\n" + "uniform mat4 model;\n" + "uniform mat4 view;\n" + "uniform mat4 projection;\n" + "\n" + "void main() {\n" + "    vertexColor = color;\n" + "    mat4 mvp = projection * view * model;\n" + "    gl_Position = mvp * vec4(position, 1.0);\n" + "}");
+		GL20.glCompileShader(vertexShader);
 
-		this.width = width;
-		this.height = height;
+		if (GL20.glGetShaderi(vertexShader, GL20.GL_COMPILE_STATUS) != GL11.GL_TRUE)
+		{
+			throw new RuntimeException(GL20.glGetShaderInfoLog(vertexShader));
+		}
+
+		int fragmentShader = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+		GL20.glShaderSource(fragmentShader, "#version 150 core\n" + "\n" + "in vec3 vertexColor;\n" + "\n" + "out vec4 fragColor;\n" + "\n" + "void main() {\n" + "    fragColor = vec4(vertexColor, 1.0);\n" + "}");
+		GL20.glCompileShader(fragmentShader);
+
+		if (GL20.glGetShaderi(fragmentShader, GL20.GL_COMPILE_STATUS) != GL11.GL_TRUE)
+		{
+			throw new RuntimeException(GL20.glGetShaderInfoLog(fragmentShader));
+		}
+
+		program = GL20.glCreateProgram();
+		GL20.glAttachShader(program, vertexShader);
+		GL20.glAttachShader(program, fragmentShader);
+		GL20.glBindAttribLocation(program, 0, "fragColor");
+		GL20.glLinkProgram(program);
+
+		if (GL20.glGetProgrami(program, GL20.GL_LINK_STATUS) != GL11.GL_TRUE)
+		{
+			throw new RuntimeException(GL20.glGetProgramInfoLog(program));
+		}
+
+		int floatSize = 4;
+
+		int posAttrib = GL20.glGetAttribLocation(program, "position");
+		GL20.glEnableVertexAttribArray(posAttrib);
+		GL20.glVertexAttribPointer(posAttrib, 3, GL11.GL_FLOAT, false, 6 * floatSize, 0);
+
+		int colAttrib = GL20.glGetAttribLocation(program, "color");
+		GL20.glEnableVertexAttribArray(colAttrib);
+		GL20.glVertexAttribPointer(colAttrib, 3, GL11.GL_FLOAT, false, 6 * floatSize, 3 * floatSize);
+
+		int uniModel = GL20.glGetUniformLocation(program, "model");
+		Matrix4f model = new Matrix4f();
+		GL20.glUniformMatrix4fv(uniModel, false, model.getBuffer());
+
+		int uniView =  GL20.glGetUniformLocation(program, "view");
+		Matrix4f view = new Matrix4f();
+		GL20.glUniformMatrix4fv(uniView, false, view.getBuffer());
+
+		int uniProjection =  GL20.glGetUniformLocation(program, "projection");
+		float ratio = 640f / 480f;
+		Matrix4f projection = Matrix4f.orthographic(-ratio, ratio, -1f, 1f, -1f, 1f);
+		GL20.glUniformMatrix4fv(uniProjection, false, projection.getBuffer());
 
 		gameLoop();
-
-		example.delete();
-
-		renderer.dispose();
 
 		GLFW.glfwDestroyWindow(window);
 		keyCallback.free();
 
 		GLFW.glfwTerminate();
+
 		errorCallback.free();
 	}
 
@@ -121,8 +171,6 @@ public class Automania
 		float accumulator = 0f;
 		float interval = 1f / TARGET_TPS;
 		float alpha;
-
-		GL11.glClearColor(0.5f, 0.5f, 0.5f, 1f);
 
 		boolean running = true;
 
@@ -187,14 +235,31 @@ public class Automania
 
 	private void render(float alpha)
 	{
-		renderer.clear();
+		/*
+		float ratio;
+		GLFW.glfwGetFramebufferSize(window, width, height);
+		ratio = width.get() / (float) height.get();
 
-		example.bind();
-		renderer.begin();
+		width.rewind();
+		height.rewind();
 
-		renderer.drawTextureRegion(example, 0, 0, 0, 0, width, height, Color.RED);
+		GL11.glViewport(0, 0, width.get(), height.get());
+		*/
 
-		renderer.end();
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
+	//	GL20.glUseProgram(program);
+
+		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3);
+
+		GLFW.glfwSwapBuffers(window);
+		GLFW.glfwPollEvents();
+
+		/*
+		width.flip();
+		height.flip();
+		*/
 	}
 
 }
