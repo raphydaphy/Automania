fa037cab2aa524b6bc71c90e15bc8fc712343f9c
raphydaphy <ster2002>
2018-03-15 21:09:34 +1100

    i chang one varibabl boi

---
+++ Assets/Scripts/LayeredNoise.cs
@@ -0,0 +1,150 @@
+﻿using System.Security.Policy;
+using UnityEditor.Graphs;
+using UnityEngine;
+
+public static class LayeredNoise 
+{
+    private static OpenSimplexNoise noise = new OpenSimplexNoise(0);
+
+    public static float[,,] GeneratePointCloud(int size, long seed, float scale, int octaves, float persistance,
+        float lacunarity, Vector3 offset)
+    {
+        if (seed != noise.GetSeed())
+        {
+            noise = new OpenSimplexNoise(seed);
+        }
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+        
+        var rand = new System.Random(OpenSimplexNoise.FastFloor(seed));
+        var octaveOffsets = new Vector3[octaves];
+
+        float maxHeight = 0;
+        float amplitude = 1;
+        
+        for (var i = 0; i < octaves; i++)
+        {
+            var offX = rand.Next(-100000, 100000) + offset.x;
+            var offY = rand.Next(-100000, 100000) - offset.y;
+            var offZ = rand.Next(-100000, 100000) + offset.z;
+            octaveOffsets[i] = new Vector3(offX, offY, offZ);
+
+            maxHeight += amplitude;
+            amplitude *= persistance;
+        }
+        
+        var noiseMap = new float[size, size, size];
+
+        var halfSize = size / 2f;
+
+        for (var z = 0; z < size; z++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                for (var x = 0; x < size; x++)
+                {
+                    amplitude = 1;
+                    float frequency = 1;
+                    float noiseHeight = 0;
+
+                    for (var i = 0; i < octaves; i++)
+                    {
+                        var sampleX = (x - halfSize + octaveOffsets[i].x) / scale * frequency;
+                        var sampleY = (y - halfSize + octaveOffsets[i].y) / scale * frequency;
+                        var sampleZ = (z - halfSize + octaveOffsets[i].z) / scale * frequency;
+                        var perlinValue = (float) noise.Evaluate(sampleX, sampleY, sampleZ) * 2 - 1;
+                        noiseHeight += perlinValue * amplitude;
+
+                        amplitude *= persistance;
+                        frequency *= lacunarity;
+                    }
+
+                    noiseMap[x, y, z] = noiseHeight;
+                }
+            }
+        }
+
+        for (var z = 0; z < size; z++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                for (var x = 0; x < size; x++)
+                {
+                    var normalizedSize = (noiseMap[x, y, z] + 1) / maxHeight;
+                    noiseMap[x, y, z] = Mathf.Clamp(normalizedSize, 0, int.MaxValue);
+                }
+            }
+        }
+
+        return noiseMap;
+    }
+    
+    public static float[,] GenerateNoiseMap(int size, long seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        if (seed != noise.GetSeed())
+        {
+            noise = new OpenSimplexNoise(seed);
+        }
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+        
+        var rand = new System.Random(OpenSimplexNoise.FastFloor(seed));
+        var octaveOffsets = new Vector2[octaves];
+
+        float maxHeight = 0;
+        float amplitude = 1;
+        
+        for (var i = 0; i < octaves; i++)
+        {
+            var offX = rand.Next(-100000, 100000) + offset.x;
+            var offY = rand.Next(-100000, 100000) - offset.y;
+            octaveOffsets[i] = new Vector2(offX, offY);
+
+            maxHeight += amplitude;
+            amplitude *= persistance;
+        }
+        
+        var noiseMap = new float[size, size];
+
+        var halfWidth = size / 2f;
+        var halfHeight = size / 2f;
+
+        for (var y = 0; y < size; y++)
+        {
+            for (var x = 0; x < size; x++)
+            {
+                amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+                
+                for (var i = 0; i < octaves; i++)
+                {
+                    var sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
+                    var sampleY = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
+                    var perlinValue =  (float)Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+                
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (var y = 0; y < size; y++)
+        {
+            for (var x = 0; x < size; x++)
+            {
+                var normalizedHeight = (noiseMap[x, y] + 1) / maxHeight;
+                noiseMap[x, y] = Mathf.Clamp(normalizedHeight, 0, int.MaxValue);
+            }
+        }
+
+        return noiseMap;
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+	public int Size = 32;
+	public long Seed;
+	public float Surface = 0.5f;
+
+	public bool PointCloudMode;
+	
+	private List<GameObject> _meshes = new List<GameObject>();
+	
+	private void Start ()
+	{
+		var pointCloud = LayeredNoise.GeneratePointCloud(Size, Seed, 10, 5, 0.2f, 2, new Vector3(0, 0, 0));
+		var voxels = new float[Size * Size * Size];
+		for (var x = 0; x < Size; x++)
+		{
+			for (var y = 0; y < Size; y++)
+			{
+				for (var z = 0; z < Size; z++)
+				{
+					var noiseVal = pointCloud[x, y, z];
+
+					if (PointCloudMode)
+					{
+						if (noiseVal > Surface)
+						{
+							MakeCube(x, y, z, noiseVal);
+						}
+					}
+					else
+					{
+						voxels[x + y * Size + z * Size * Size] = noiseVal;
+					}
+				}
+			}
+		}
+
+		if (PointCloudMode)
+		{
+			return;
+		}
+
+		var vertices = new List<Vector3>();
+		var indices = new List<int>();
+		
+		var generator = new MarchingCubesGenerator(Surface);
+		
+		generator.Generate(voxels, Size, Size, Size, vertices, indices);
+		
+		var maxVertsPerMesh = 30000; //must be divisible by 3, ie 3 verts == 1 triangle
+		var numMeshes = vertices.Count / maxVertsPerMesh + 1;
+
+		for (var i = 0; i < numMeshes; i++)
+		{
+
+			var splitVerts = new List<Vector3>();
+			var splitIndices = new List<int>();
+
+			for (var j = 0; j < maxVertsPerMesh; j++)
+			{
+				var idx = i * maxVertsPerMesh + j;
+
+				if (idx < vertices.Count)
+				{
+					splitVerts.Add(vertices[idx]);
+					splitIndices.Add(j);
+				}
+			}
+
+			if (splitVerts.Count == 0) continue;
+
+			var mesh = new Mesh();
+			mesh.SetVertices(splitVerts);
+			mesh.SetTriangles(splitIndices, 0);
+			mesh.RecalculateBounds();
+			mesh.RecalculateNormals();
+
+			var go = new GameObject("Mesh");
+			go.transform.parent = transform;
+			go.AddComponent<MeshFilter>();
+			go.AddComponent<MeshRenderer>();
+			go.GetComponent<Renderer>().material.color = Color.blue;
+			go.GetComponent<MeshFilter>().mesh = mesh;
+			go.transform.localPosition = new Vector3(-Size / 2, -Size / 2, -Size / 2);
+
+			_meshes.Add(go);
+		}
+	}
+
+	private void MakeCube(float gridX, float gridY, float gridZ, float scale)
+	{
+		scale *= 100;
+		var plane = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		plane.transform.parent = gameObject.transform;
+		plane.name = "Voxel";
+		plane.GetComponent<Renderer>().material.color = Color.blue;
+		plane.transform.position = new Vector3(gridX * 100, gridY * 100, gridZ * 100);
+		plane.transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
